ac872babc014ff1401d1dd2d3de91df074fd0197
Praveen Dubbaka <praveendubbaka@example.com>
2026-01-28 14:50:03 -0500

    Update base path to root for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bawarchi-toronto-website/', // Replace with your repository name
+  base: '/', // Custom domain serves from root
   build: {
     outDir: 'dist',
     sourcemap: false,
